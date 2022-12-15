851367dcce8cbdd9cbca64442853d53904493009
unknown <m1999@live.jp>
2022-12-16 01:16:31 +0900

    Update docker-setup.sh

--- docker-setup.sh
+++ docker-setup.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 sudo apt -qq install -y ca-certificates curl gnupg lsb-release
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
